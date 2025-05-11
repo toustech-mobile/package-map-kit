dad3fcf0188640bfa6b228db1048a834720bacd0
Mohammad Barati <mohammad_specter@yahoo.com>
2025-05-11 11:35:01 +0330

    fix my current location button position

--- lib/widgets/flutter/flutter_map_widget.dart
+++ lib/widgets/flutter/flutter_map_widget.dart
@@ -99,18 +99,24 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
     }
 
     return Scaffold(
-      body: _buildFlutterMap(),
-      floatingActionButton: widget.isCurrentLocationEnable ?? false
-          ? FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: _moveToUserLocation,
-              child: Icon(
-                Icons.my_location,
-                color: widget.userMarker != null ? Colors.blue : Colors.grey,
+      body: Stack(
+        children: [
+          _buildFlutterMap(),
+          if (widget.isCurrentLocationEnable ?? false)
+            Positioned(
+              right: 24,
+              bottom: 24,
+              child: FloatingActionButton(
+                backgroundColor: Colors.white,
+                onPressed: _moveToUserLocation,
+                child: Icon(
+                  Icons.my_location,
+                  color: widget.userMarker != null ? Colors.blue : Colors.grey,
+                ),
               ),
-            )
-          : null,
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+            ),
+        ],
+      ),
     );
   }
 
