7fd6f798a0b76ec9e00849a652f6399a72ddf28c
Mohammad Barati <mohammad_specter@yahoo.com>
2025-11-04 10:48:23 +0330

    fix: improve circle tap detection and event handling
    This commit refines the tap detection logic for circles on the map. The `onTap` callback is now consistently triggered alongside `onCircleTap` when a circle is tapped, ensuring both events are fired as expected.
    
    Additionally, the code has been reformatted for better readability and consistency.

--- lib/widgets/flutter/flutter_map_widget.dart
+++ lib/widgets/flutter/flutter_map_widget.dart
@@ -78,8 +78,7 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
           if (!mounted) return;
           widget.circles!.addAll(circles);
           // also add new hidden markers for the new circles
-          _circleMarkers.addAll(circles.map((circle) =>
-              MarkerModel(
+          _circleMarkers.addAll(circles.map((circle) => MarkerModel(
                 latitude: circle.latitude,
                 longitude: circle.longitude,
                 data: '',
@@ -199,7 +198,7 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
       children: [
         TileLayer(
           urlTemplate:
-          widget.isDarkMode ?? false ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" : tileUrl,
+              widget.isDarkMode ?? false ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" : tileUrl,
           subdomains: const ['a', 'b', 'c'],
         ),
         PolylineLayer(
@@ -227,15 +226,14 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
   void _initializeHiddenMarkers() {
     _circleMarkers.addAll(
       widget.circles!.map(
-            (circle) =>
-            MarkerModel(
-              latitude: circle.latitude,
-              longitude: circle.longitude,
-              data: '',
-              icon: '',
-              snippetTitle: circle.snippetTitle,
-              snippetDescription: circle.snippetDescription,
-            ),
+        (circle) => MarkerModel(
+          latitude: circle.latitude,
+          longitude: circle.longitude,
+          data: '',
+          icon: '',
+          snippetTitle: circle.snippetTitle,
+          snippetDescription: circle.snippetDescription,
+        ),
       ),
     );
   }
@@ -280,11 +278,10 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
         popupDisplayOptions: PopupDisplayOptions(
           builder: (BuildContext context, Marker marker) {
             final tappedMarker = combinedMarkers.firstWhere(
-                  (m) =>
-              m.latitude == marker.point.latitude && m.longitude == marker.point.longitude && m.icon.isNotEmpty,
+              (m) => m.latitude == marker.point.latitude && m.longitude == marker.point.longitude && m.icon.isNotEmpty,
               orElse: () {
                 final tappedMarker = combinedMarkers.firstWhere((m) =>
-                m.latitude == marker.point.latitude && m.longitude == marker.point.longitude && m.icon.isEmpty);
+                    m.latitude == marker.point.latitude && m.longitude == marker.point.longitude && m.icon.isEmpty);
                 return MarkerModel(
                     latitude: marker.point.latitude,
                     longitude: marker.point.longitude,
@@ -302,7 +299,7 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
                 widget.onMarkerTap?.call(tappedMarker);
               } else {
                 final tappedCircle = widget.circles!.firstWhere(
-                      (c) => c.latitude == marker.point.latitude && c.longitude == marker.point.longitude,
+                  (c) => c.latitude == marker.point.latitude && c.longitude == marker.point.longitude,
                 );
                 widget.onCircleTap?.call(tappedCircle);
               }
@@ -352,18 +349,27 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
     widget.popupController.hideAllPopups();
     _lastTappedMarker = null;
     _popupVisible = false;
+
     final circle = widget.circles?.firstWhere(
-          (circle) =>
-      const Distance().as(LengthUnit.Meter, LatLng(circle.latitude, circle.longitude), point) <= circle.radius,
-      orElse: () =>
-          CircleModel(latitude: 0,
-          longitude: 0,
-          radius: 0,
-          color: Colors.red,
-          borderColor: Colors.red),
+      (circle) =>
+          const Distance().as(
+            LengthUnit.Meter,
+            LatLng(circle.latitude, circle.longitude),
+            point,
+          ) <=
+          circle.radius,
+      orElse: () => CircleModel(
+        latitude: 0,
+        longitude: 0,
+        radius: 0,
+        color: Colors.red,
+        borderColor: Colors.red,
+      ),
     );
-    if (circle!.longitude != 0) {
+
+    if (circle != null && circle.longitude != 0) {
       widget.onCircleTap?.call(circle);
+      widget.onTap?.call(point);
     } else {
       widget.onTap?.call(point);
     }
