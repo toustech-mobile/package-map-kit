34a038dd540da09593a79ee560ffc51a3add1af6
aliyou <a.yousefi199@gmail.com>
2026-06-01 13:29:15 +0330

    refactor: move polyline decoding logic to PolyLinePointModel
    - Relocated the polyline decoding implementation from `PolyLineModel` to a static `decodePoints` method in `PolyLinePointModel`.
    - Updated the `PolyLineModel.decodePoints` constructor to use the new centralized decoding method.
    - Removed the redundant private `_decode` method from `PolyLineModel`.

--- ios/map_kit/Package.swift
+++ ios/map_kit/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .iOS("13.0") // Update this to match the minimum iOS version in your podspec
     ],
     products: [
-        .library(name: "map_kit", targets: ["map_kit"])
+        .library(name: "map-kit", targets: ["map_kit"])
     ],
     dependencies: [
         .package(name: "FlutterFramework", path: "../FlutterFramework")
